87f3337316ab8a52cd430bc466572921cfa3e9ca
notFreak <nikitapolovin2003@gmail.com>
2024-10-29 20:06:54 +0700

    Переход от тривиального автомата к лексическому

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            StateMachineService stateMachineService = new StateMachineService(MyStateMachine.class, "State1");
+            StateMachineService stateMachineService = new StateMachineService(MyStateMachine.class, "START");
+            String input = "var123 456";
 
-            stateMachineService.processEvent("Event1");
-            stateMachineService.processEvent("Event2");
-            stateMachineService.processEvent("Event3");
+            for (char c : input.toCharArray()) {
+                String event = getEventType(c);
+                stateMachineService.processEvent(event, c);
+            }
+            stateMachineService.processEvent("OTHER", ' ');
+
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String getEventType(char c) {
+        if (Character.isLetter(c)) {
+            return "LETTER";
+        } else if (Character.isDigit(c)) {
+            return "DIGIT";
+        } else if (Character.isLetterOrDigit(c)) {
+            return "LETTER_OR_DIGIT";
+        } else {
+            return "OTHER";
         }
     }
 }
--- src/StateMachineService.java
+++ src/StateMachineService.java
@@ -3,31 +3,51 @@ import java.lang.reflect.Method;
 public class StateMachineService {
     private Object target;
     private String currentState;
+    private boolean isRunning = true;
 
     public StateMachineService(Class<?> clazz, String initialState) throws Exception {
         this.target = clazz.getDeclaredConstructor().newInstance();
         this.currentState = initialState;
     }
 
-    public void processEvent(String event) throws Exception {
+    public void processEvent(String event, char currentChar) throws Exception {
+        if (!isRunning) {
+            throw new IllegalStateException("Автомат остановлен из-за ошибки.");
+        }
+
         Class<?> clazz = target.getClass();
         Method[] methods = clazz.getMethods();
+
+        boolean transitionFound = false;
 
         for (Method method : methods) {
             if (method.isAnnotationPresent(Transition.class)) {
                 Transition transition = method.getAnnotation(Transition.class);
                 if (transition.fromState().equals(currentState) && transition.event().equals(event)) {
-                    System.out.println("Текущее состояние: " + currentState);
+                    Method setCharMethod = clazz.getMethod("setCurrentChar", char.class);
+                    setCharMethod.invoke(target, currentChar);
+
+                    System.out.println("Текущее состояние: " + currentState + ", Событие: " + event);
                     Method targetMethod = clazz.getMethod(transition.methodName());
                     targetMethod.setAccessible(true);
                     targetMethod.invoke(target);
                     currentState = transition.toState();
+
                     System.out.println("Переход в состояние: " + currentState + "\n");
-                    return;
+                    transitionFound = true;
+                    break;
                 }
             }
         }
 
-        throw new IllegalArgumentException("Неверный переход для события: " + event);
+        if (!transitionFound) {
+            isRunning = false;
+            throw new IllegalArgumentException("Некорректный переход для события: " + event + " в состоянии: " + currentState);
+        }
+
+        if (currentState.equals("END")) {
+            isRunning = false;
+            System.out.println("Автомат достиг конечного состояния.");
+        }
     }
 }
